95d87926d9cbfa04c30dfbd5ce1d9ec868e71e3f
calvinvanoss <91079027+calvinvanoss@users.noreply.github.com>
2024-02-05 18:04:04 -0800

    Add binopC

--- assignment 3.rkt
+++ assignment 3.rkt
@@ -2,12 +2,9 @@
 
 (require typed/rackunit)
 
-(define-type ExprC (U numC plusC subC multC divC idC appC ifleq0?))
+(define-type ExprC (U numC binopC idC appC ifleq0?))
 (struct numC ([n : Real]) #:transparent)
-(struct plusC ([l : ExprC][r : ExprC]) #:transparent)
-(struct subC ([l : ExprC][r : ExprC]) #:transparent)
-(struct multC ([l : ExprC][r : ExprC]) #:transparent)
-(struct divC ([l : ExprC][r : ExprC]) #:transparent)
+(struct binopC ([s : Symbol][l : ExprC][r : ExprC]) #:transparent)
 (struct idC ([s : Symbol]) #:transparent)
 (struct appC ([fun : Symbol][arg : ExprC]) #:transparent)
 (struct ifleq0? ([test : ExprC] [if : ExprC] [else : ExprC]) #:transparent)
@@ -17,31 +14,21 @@
 ; parse Sexp to ExprC
 (: parse (-> Sexp ExprC))
 (define (parse exp)
-  ; parse binop into ExprC
-  (: parse-binop (-> Symbol Sexp Sexp ExprC))
-  (define (parse-binop op l r)
-    (match op
-      ['+ (plusC (parse l) (parse r))]
-      ['- (subC (parse l) (parse r))]
-      ['* (multC (parse l) (parse r))]
-      ['/ (divC (parse l) (parse r))]
-      [else (error "OAZO parse-binop failure")]))
   (match exp
-    [(list (? symbol? op) l r) (parse-binop op l r)]
+    [(list (? symbol? op) l r) (binopC op (parse l) (parse r))]
     [(list 'ifleq0? test if else) (ifleq0? (parse test) (parse if) (parse else))]
     [(? real? n) (numC n)]
     [(? symbol? s) (idC s)]
     [(list (? symbol? fun) (? real? arg)) (appC fun (parse arg))]
     [else (error "OAZO parse failure")]))
 
-(check-equal? (parse '{+ x 14}) (plusC (idC 'x) (numC 14)))
-(check-equal? (parse '{- x 14}) (subC (idC 'x) (numC 14)))
-(check-equal? (parse '{* x 14}) (multC (idC 'x) (numC 14)))
-(check-equal? (parse '{/ x 14}) (divC (idC 'x) (numC 14)))
+(check-equal? (parse '{+ x 14}) (binopC '+ (idC 'x) (numC 14)))
+(check-equal? (parse '{- x 14}) (binopC '- (idC 'x) (numC 14)))
+(check-equal? (parse '{* x 14}) (binopC '* (idC 'x) (numC 14)))
+(check-equal? (parse '{/ x 14}) (binopC '/ (idC 'x) (numC 14)))
 (check-equal? (parse '{f 2}) (appC 'f (numC 2)))
 (check-equal? (parse '{ifleq0? 1 1 0}) (ifleq0? (numC 1) (numC 1) (numC 0)))
 (check-exn (regexp (regexp-quote "OAZO parse failure")) (lambda () (parse '("test fail"))))
-(check-exn (regexp (regexp-quote "OAZO parse-binop failure")) (lambda () (parse '(k "test" "failure"))))
 
 ; parse Sexp into fundefC
 (: parse-fundef (-> Sexp fundefC))
@@ -50,7 +37,7 @@
     [(list 'func (list(? symbol? name) (? symbol? arg)) ': body) (fundefC name arg (parse body))]
     [else (error "OAZO parse-fundef failure")]))
 
-(check-equal? (parse-fundef '{func {f x} : {+ x 14}}) (fundefC 'f 'x (plusC (idC 'x) (numC 14))))
+(check-equal? (parse-fundef '{func {f x} : {+ x 14}}) (fundefC 'f 'x (binopC '+ (idC 'x) (numC 14))))
 (check-exn (regexp (regexp-quote "OAZO parse-fundef failure")) (lambda () (parse-fundef '(k "test" "failure"))))
 
 ; parses Sexp into list of fundefC
@@ -63,7 +50,7 @@
 
 (check-equal? (parse-prog '{{func {f x} : {+ x 14}}
                              {func {main init} : {f 2}}})
-              (list (fundefC 'f 'x (plusC (idC 'x) (numC 14))) (fundefC 'main 'init (appC 'f (numC 2)))))
+              (list (fundefC 'f 'x (binopC '+ (idC 'x) (numC 14))) (fundefC 'main 'init (appC 'f (numC 2)))))
 (check-exn (regexp (regexp-quote "OAZO parse-prog failure")) (lambda () (parse-prog "")))
 
 ; replace 'for' with 'what' inside the expression 'in'
@@ -75,19 +62,16 @@
                [(eq? s for) what]
                [else in])]
     [(appC fun arg) (appC fun (subst what for arg))]
-    [(plusC l r) (plusC (subst what for l) (subst what for r))]
-    [(subC l r) (subC (subst what for l) (subst what for r))]
-    [(multC l r) (multC (subst what for l) (subst what for r))]
-    [(divC l r) (divC (subst what for l) (subst what for r))]
+    [(binopC op l r) (binopC op (subst what for l) (subst what for r))]
     [(ifleq0? test then else) (ifleq0? (subst what for test) (subst what for then) (subst what for else))]))
 
-(check-equal? (subst (numC 4) 'this (plusC (numC 3) (idC 'this))) (plusC (numC 3) (numC 4)))
-(check-equal? (subst (numC 3) 'this (plusC (numC 3) (idC 'notthis))) (plusC (numC 3) (idC 'notthis)))
+(check-equal? (subst (numC 4) 'this (binopC '+ (numC 3) (idC 'this))) (binopC '+ (numC 3) (numC 4)))
+(check-equal? (subst (numC 3) 'this (binopC '+ (numC 3) (idC 'notthis))) (binopC '+ (numC 3) (idC 'notthis)))
 (check-equal? (subst (numC 3) 'f (numC 4)) (numC 4))
 (check-equal? (subst (numC 3) 'f (appC 'g (idC 'f))) (appC 'g (numC 3))) 
-(check-equal? (subst (numC 3) 'f (subC (numC 1) (numC 2))) (subC (numC 1) (numC 2)))
-(check-equal? (subst (numC 3) 'f (multC (numC 1) (numC 2))) (multC (numC 1) (numC 2)))
-(check-equal? (subst (numC 3) 'f (divC (numC 1) (numC 2))) (divC (numC 1) (numC 2)))
+(check-equal? (subst (numC 3) 'f (binopC '- (numC 1) (numC 2))) (binopC '- (numC 1) (numC 2)))
+(check-equal? (subst (numC 3) 'f (binopC '* (numC 1) (numC 2))) (binopC '* (numC 1) (numC 2)))
+(check-equal? (subst (numC 3) 'f (binopC '/ (numC 1) (numC 2))) (binopC '/ (numC 1) (numC 2)))
 
 ; gets fundef body given name of fundef and list of fundefs
 (: get-fundef (-> Symbol (Listof fundefC) ExprC))
@@ -98,7 +82,8 @@
                   [else (get-fundef name r)])]
     [else (error "OAZO get-fundef failure")]))
 
-(check-equal? (get-fundef 'name (list (fundefC 'name 'arg (plusC (numC 1) (numC 2))))) (plusC (numC 1) (numC 2)))
+(check-equal? (get-fundef 'name (list (fundefC 'name 'arg (binopC '+ (numC 1) (numC 2)))))
+              (binopC '+ (numC 1) (numC 2)))
 (check-exn (regexp (regexp-quote "OAZO get-fundef failure"))
            (lambda () (get-fundef 'f (list (fundefC 'name 'arg (numC 4))))))
 
@@ -111,7 +96,7 @@
                   [else (get-fundef-arg name r)])]
     [else (error "OAZO get-fundef-arg failure")]))
 
-(check-equal? (get-fundef-arg 'name (list (fundefC 'name 'arg (plusC (numC 1) (numC 2))))) 'arg)
+(check-equal? (get-fundef-arg 'name (list (fundefC 'name 'arg (binopC '+ (numC 1) (numC 2))))) 'arg)
 (check-exn (regexp (regexp-quote "OAZO get-fundef-arg failure"))
            (lambda () (get-fundef-arg 'f (list (fundefC 'name 'arg (numC 4))))))
 
@@ -120,18 +105,22 @@
 (define (interp exprc fds)
   (match exprc
     [(numC n) n]
-    [(plusC l r) (+ (interp l fds) (interp r fds))]
-    [(subC l r) (- (interp l fds) (interp r fds))]
-    [(multC l r) (* (interp l fds) (interp r fds))]
-    [(divC l r) (/ (interp l fds) (interp r fds))]
+    [(binopC op l r) (match op
+                       ['+ (+ (interp l fds) (interp r fds))]
+                       ['- (- (interp l fds) (interp r fds))]
+                       ['* (* (interp l fds) (interp r fds))]
+                       ['/ (/ (interp l fds) (interp r fds))]
+                       [else (error "OAZO invalid op")])]
     [(appC fun arg) (interp (subst arg (get-fundef-arg fun fds) (get-fundef fun fds)) fds)]
     [(ifleq0? test then else) (if (<= 0 (interp test fds)) (interp then fds) (interp else fds))]))
 
-(check-equal? (interp (plusC (numC 1) (numC 2)) '()) 3)
-(check-equal? (interp (subC (numC 7) (numC 3)) '()) 4)
-(check-equal? (interp (multC (numC 7) (numC 3)) '()) 21)
-(check-equal? (interp (divC (numC 9) (numC 3)) '()) 3)
-(check-equal? (interp (appC 'f (numC 2)) (list (fundefC 'f 'x (plusC (idC 'x) (numC 14))))) 16)
+(check-equal? (interp (binopC '+ (numC 1) (numC 2)) '()) 3)
+(check-equal? (interp (binopC '- (numC 7) (numC 3)) '()) 4)
+(check-equal? (interp (binopC '* (numC 7) (numC 3)) '()) 21)
+(check-equal? (interp (binopC '/ (numC 9) (numC 3)) '()) 3)
+(check-exn (regexp (regexp-quote "OAZO invalid op"))
+           (lambda () (interp (binopC 'fail (numC 1) (numC 2)) '())))
+(check-equal? (interp (appC 'f (numC 2)) (list (fundefC 'f 'x (binopC '+ (idC 'x) (numC 14))))) 16)
 
 ; interprets the function named main using the list of function definitions
 (: interp-fns (-> (Listof fundefC) Real))
@@ -140,7 +129,7 @@
     [(empty? fds) (error "OAZO main is empty")]
     [else (interp (get-fundef 'main fds) fds)]))
 
-(check-equal? (interp-fns (list (fundefC 'main 'init (plusC (numC 2) (numC 2))))) 4)
+(check-equal? (interp-fns (list (fundefC 'main 'init (binopC '+ (numC 2) (numC 2))))) 4)
 (check-exn (regexp (regexp-quote "OAZO main is empty"))
            (lambda () (interp-fns '())))
 
