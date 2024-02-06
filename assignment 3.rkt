b0b8437288f2dcc75a5bab59722345857fe6ce9f
Nick Bircher <nicky.b.three@gmail.com>
2024-02-06 10:41:21 -0800

    Fully functional OAZO3 passing tests

--- assignment 3.rkt
+++ assignment 3.rkt
@@ -22,7 +22,8 @@
     [(list 'ifleq0? test if else) (ifleq0? (parse test) (parse if) (parse else))]
     [(? real? n) (numC n)]
     [(? symbol? s) (if ((make-predicate InvalidId) s) (error "OAZO invalid id") (idC s))]
-    [(list (? symbol? fun) arg) (appC fun (parse arg))]
+    [(list (? symbol? fun) arg) (if ((make-predicate Binop) fun) (error "OAZO incorrect binop arg number")
+                                    (appC fun (parse arg)))]
     [else (error "OAZO parse failure")]))
 
 (check-equal? (parse '{+ x 14}) (binopC '+ (idC 'x) (numC 14)))
@@ -33,6 +34,7 @@
 (check-equal? (parse '{ifleq0? 1 1 0}) (ifleq0? (numC 1) (numC 1) (numC 0)))
 (check-exn (regexp (regexp-quote "OAZO parse failure")) (lambda () (parse '("test fail"))))
 (check-exn (regexp (regexp-quote "OAZO invalid id")) (lambda () (parse '(+ / 3))))
+(check-exn (regexp (regexp-quote "OAZO incorrect binop arg number")) (lambda () (parse '(+ 1))))
 
 ; parse Sexp into fundefC
 (: parse-fundef (-> Sexp fundefC))
@@ -62,24 +64,24 @@
 (check-exn (regexp (regexp-quote "OAZO parse-prog failure")) (lambda () (parse-prog "")))
 
 ; replace 'for' with 'what' inside the expression 'in'
-(: subst (-> ExprC Symbol ExprC ExprC))
+(: subst (-> Real Symbol ExprC ExprC))
 (define (subst what for in)
   (match in
     [(numC n) in]
     [(idC s) (cond
-               [(eq? s for) what]
+               [(eq? s for) (numC what)]
                [else in])]
     [(appC fun arg) (appC fun (subst what for arg))]
     [(binopC op l r) (binopC op (subst what for l) (subst what for r))]
     [(ifleq0? test then else) (ifleq0? (subst what for test) (subst what for then) (subst what for else))]))
 
-(check-equal? (subst (numC 4) 'this (binopC '+ (numC 3) (idC 'this))) (binopC '+ (numC 3) (numC 4)))
-(check-equal? (subst (numC 3) 'this (binopC '+ (numC 3) (idC 'notthis))) (binopC '+ (numC 3) (idC 'notthis)))
-(check-equal? (subst (numC 3) 'f (numC 4)) (numC 4))
-(check-equal? (subst (numC 3) 'f (appC 'g (idC 'f))) (appC 'g (numC 3))) 
-(check-equal? (subst (numC 3) 'f (binopC '- (numC 1) (numC 2))) (binopC '- (numC 1) (numC 2)))
-(check-equal? (subst (numC 3) 'f (binopC '* (numC 1) (numC 2))) (binopC '* (numC 1) (numC 2)))
-(check-equal? (subst (numC 3) 'f (binopC '/ (numC 1) (numC 2))) (binopC '/ (numC 1) (numC 2)))
+(check-equal? (subst 4 'this (binopC '+ (numC 3) (idC 'this))) (binopC '+ (numC 3) (numC 4)))
+(check-equal? (subst 3 'this (binopC '+ (numC 3) (idC 'notthis))) (binopC '+ (numC 3) (idC 'notthis)))
+(check-equal? (subst 3 'f (numC 4)) (numC 4))
+(check-equal? (subst 3 'f (appC 'g (idC 'f))) (appC 'g (numC 3))) 
+(check-equal? (subst 3 'f (binopC '- (numC 1) (numC 2))) (binopC '- (numC 1) (numC 2)))
+(check-equal? (subst 3 'f (binopC '* (numC 1) (numC 2))) (binopC '* (numC 1) (numC 2)))
+(check-equal? (subst 3 'f (binopC '/ (numC 1) (numC 2))) (binopC '/ (numC 1) (numC 2)))
 
 ; gets fundef body given name of fundef and list of fundefs
 (: get-fundef (-> Symbol (Listof fundefC) fundefC))
@@ -104,14 +106,20 @@
                        ['+ (+ (interp l fds) (interp r fds))]
                        ['- (- (interp l fds) (interp r fds))]
                        ['* (* (interp l fds) (interp r fds))]
-                       ['/ (/ (interp l fds) (interp r fds))]
+                       ['/ (let ([denominator (interp r fds)])
+                             (if (eq? denominator 0) (error "OAZO /0") (/ (interp l fds) denominator)))]
                        [else (error "OAZO invalid op")])]
-    [(appC fun arg) (let ([fd (get-fundef fun fds)]) (interp (subst arg (fundefC-arg fd) (fundefC-body fd)) fds))]
+    [(appC fun arg) (let ([fd (get-fundef fun fds)])
+                      (interp (subst (interp arg fds) (fundefC-arg fd) (fundefC-body fd)) fds))]
     [(ifleq0? test then else) (if (>= 0 (interp test fds)) (interp then fds) (interp else fds))]
     [(idC _) (error "OAZO interp shouldn't get here")]))
 
 (check-= (interp (ifleq0? (numC -1) (numC 0) (numC 1)) '()) 0 0)
 (check-= (interp (ifleq0? (binopC '* (numC 3) (numC 1)) (numC 0) (numC 1)) '()) 1 0)
+(check-exn (regexp (regexp-quote "OAZO /0"))
+           (lambda () (interp (binopC '/ (numC 5) (numC 0)) '())))
+(check-exn (regexp (regexp-quote "OAZO /0"))
+           (lambda () (interp (binopC '/ (numC 5) (binopC '+ (numC 0) (numC 0))) '())))
 
 (check-equal? (interp (binopC '+ (numC 1) (numC 2)) '()) 3)
 (check-equal? (interp (binopC '- (numC 7) (numC 3)) '()) 4)
